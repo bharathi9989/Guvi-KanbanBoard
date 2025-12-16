41e325faa7ea2782f3fdc60d56359d3e28a0facc
bharathi9989 <er.velubharathi.s@gmail.com>
2025-12-16 17:51:31 +0530

    tailwind update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 function App() {
-  return <div>Kanban Board</div>;
+  return (<div>
+    <h1 className="text-red-600 bg-amber-500 text-center font-bold">Kanban-Board</h1>
+  </div>)
 }
-
 export default App;
