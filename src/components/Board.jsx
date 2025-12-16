290c4f0d57c4f7def97879950be2b2b5a2823244
bharathi9989 <er.velubharathi.s@gmail.com>
2025-12-16 18:56:12 +0530

    updated components

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Column from "./Column";
 
+const COLUMNS = [
+  { id: "todo", title: "To Do" },
+  { id: "inprogress", title: "In Progress" },
+  { id: "done", title: "Done" },
+];
 function Board() {
   return (
-    <div>Board</div>
-  )
+    <div className="">
+      {COLUMNS.map((col) => (
+        <Column key={col.id} status={col.id} title={col.title} />
+      ))}
+    </div>
+  );
 }
 
-export default Board
+export default Board;
--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useTasks } from "../context/TaskContext";
+import TaskCard from "./TaskCard";
 
 function Column() {
+  const { tasks } = useTasks();
+
+  const filterdTasks = tasks.filter((task) => task.status === status);
   return (
-    <div>Column</div>
-  )
+    <div className="bg-zinc-800 rounded-xl p-4 min-h-[500px]">
+      <h2 className="text-xl font-bold text-white mb-4 flex justify-between">
+        {title}
+        <span className="text-sm bg-zinc-700 px-2 py-1 rounded-full">
+          {filterdTasks.length}
+        </span>
+      </h2>
+      <div className="space-y-3">
+        {filterdTasks.map((task) => (
+          <TaskCard key={task.id} task={tasks} />
+        ))}
+
+        {filterdTasks.length === 0 && (
+          <p className="text-zinc-500 text-sm italic text-center mt-10">
+            No Tasks
+          </p>
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default Column
+export default Column;
